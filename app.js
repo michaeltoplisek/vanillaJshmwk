a3f34ab99b684ac6a242f79eed1bba7407a4e238
Tops <mtoplisek95@gmail.com>
2018-10-06 17:36:29 -0400

    got the view button working and started the add button

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-$(".view").on("click", $(".viewMaincontain").toggleClass("viewMaincontain").append(`<div>${employeeList}</div>`));
+$(".view").on("click", function () {
+    $(".mainContain").empty();
+    for (let i = 0; i < employeeList.length; i++) {
+        const nameShow = employeeList[i].name
+        const officeShow = employeeList[i].officeNum
+        const phoneShow = employeeList[i].phoneNum
+        $(".viewBtn").append(`<p>Name: ${nameShow}<br />Office Number: ${officeShow}<br />Phone Number: ${phoneShow}</p>`);
+    }
+})
+
+$(".add").on("click", function () {
+    $(".mainContain").empty();
+    for (let i = 0; i < employeeList.length; i++) {
+        const nameShow = employeeList[i].name
+        const officeShow = employeeList[i].officeNum
+        const phoneShow = employeeList[i].phoneNum
+        $(".addBtn").append(`<p>Name: ${nameShow}<br />Office Number: ${officeShow}<br />Phone Number: ${phoneShow}</p>`);
+    }
+    const newName = $(".nameInput").val()
+    const newOffice = $(".officeInput").val()
+    const newPhone = $(".phoneInput").val()
+    const newEmployee = {
+        "name": newName,
+        "officeNum": newOffice,
+        "phoneNum": newPhone,
+    }
+    employeeList.push(newEmployee)
+})
