5fc4a9e9e12022f0c4f5c480e320b8f831df5431
Tops <mtoplisek95@gmail.com>
2018-10-06 22:24:19 -0400

    completed the verify view page

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const showView = function() {
+const showView = function () {
     $(".viewBtn").empty()
     $("#viewID").removeClass("hide")
     $("#addID").addClass("hide")
@@ -6,11 +6,12 @@ const showView = function() {
     $("#updateID").addClass("hide")
     $("#deleteID").addClass("hide")
     $("#inputLine").addClass("hide")
+    $("#verifyTextID").addClass("hide")
     for (let i = 0; i < employeeList.length; i++) {
         const nameShow = employeeList[i].name
         const officeShow = employeeList[i].officeNum
         const phoneShow = employeeList[i].phoneNum
-        $(".viewBtn").append(`<p>Name: ${nameShow}<br />Office Number: ${officeShow}<br />Phone Number: ${phoneShow}</p>`);
+        $(".viewBtn").append(`<div><p>Name: ${nameShow}<br />Office Number: ${officeShow}<br />Phone Number: ${phoneShow}</p></div>`);
     }
 }
 
@@ -23,13 +24,14 @@ const showAdd = function () {
     $("#verifyID").addClass("hide")
     $("#updateID").addClass("hide")
     $("#deleteID").addClass("hide")
-    $("#inputLine").removeClass("hide") 
+    $("#inputLine").removeClass("hide")
+    $("#verifyTextID").addClass("hide")
     for (let i = 0; i < employeeList.length; i++) {
         const nameShow = employeeList[i].name
         const officeShow = employeeList[i].officeNum
         const phoneShow = employeeList[i].phoneNum
-        $(".addBtn").append(`<p>Name: ${nameShow}<br />Office Number: ${officeShow}<br />Phone Number: ${phoneShow}</p>`);
-    }  
+        $(".addBtn").append(`<div><p>Name: ${nameShow}<br />Office Number: ${officeShow}<br />Phone Number: ${phoneShow}</p></div`);
+    }
     const newName = $(".nameInput").val()
     const newOffice = $(".officeInput").val()
     const newPhone = $(".phoneInput").val()
@@ -38,17 +40,33 @@ const showAdd = function () {
         'officeNum': `${newOffice}`,
         'phoneNum': `${newPhone}`,
     }
-    employeeList.push(`${newEmployee}`) 
+    employeeList.push(`${newEmployee}`)
 }
 
 $(".add").on("click", showAdd)
 
-const showVerify = function() {
-    $(".verifyBtn").empty()
+const showVerify = function () {
+    $("#verifyID").empty()
     $("#viewID").addClass("hide")
-    $("#addID").removeClass("hide")
+    $("#addID").addClass("hide")
     $("#verifyID").removeClass("hide")
     $("#updateID").addClass("hide")
     $("#deleteID").addClass("hide")
-    $("#inputLine").addClass("hide") 
+    $("#inputLine").addClass("hide")
+    $("#verifyTextID").removeClass("hide")
 }
+
+const verifyButton = function () {
+    $("#verifyID").empty()
+    let search = $("#verifyText").val()
+    for (let i = 0; i < employeeList.length; i++) {
+        if (search === employeeList[i].name) {
+            $("#verifyID").append("Employee exists")
+            break
+        } else {
+            $("#verifyID").append("Employee doesn't exist")
+        }
+    }
+}
+$(".verify").on("click", showVerify)
+$("#verifyButton").on("click", verifyButton)
