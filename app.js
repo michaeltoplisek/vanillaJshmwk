b644aed8835f16df8ec75d9e3cabd1c35a1f7fd4
Tops <mtoplisek95@gmail.com>
2018-10-06 23:35:42 -0400

    finished functionality of JS

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const showView = function () {
     $(".viewBtn").empty()
     $("#viewID").removeClass("hide")
@@ -61,12 +62,69 @@ const verifyButton = function () {
     let search = $("#verifyText").val()
     for (let i = 0; i < employeeList.length; i++) {
         if (search === employeeList[i].name) {
+            $("#verifyID").empty()
             $("#verifyID").append("Employee exists")
             break
         } else {
+            $("#verifyID").empty()
             $("#verifyID").append("Employee doesn't exist")
         }
     }
 }
 $(".verify").on("click", showVerify)
 $("#verifyButton").on("click", verifyButton)
+
+const showDelete = function () {
+    $("#verifyID").empty()
+    $("#viewID").addClass("hide")
+    $("#addID").addClass("hide")
+    $("#verifyID").removeClass("hide")
+    $("#updateID").addClass("hide")
+    $("#deleteID").addClass("hide")
+    $("#inputLine").addClass("hide")
+    $("#verifyTextID").removeClass("hide")
+    let search = $("#verifyText").val()
+    for (let i = 0; i < employeeList.length; i++) {
+        if (search === employeeList[i].name) {
+            console.log(i)
+            employeeList.splice(i, 1)
+        } else { }
+    }
+    for (let i = 0; i < employeeList.length; i++) {
+        const nameShow = employeeList[i].name
+        const officeShow = employeeList[i].officeNum
+        const phoneShow = employeeList[i].phoneNum
+        $(".viewBtn").append(`<div><p>Name: ${nameShow}<br />Office Number: ${officeShow}<br />Phone Number: ${phoneShow}</p></div>`);
+    }
+}
+
+$(".delete").on("click", showDelete)
+
+const showUpdate = function () {
+    $("#verifyID").empty()
+    $("addBtn").empty()
+    $(".viewBtn").empty()
+    $("#viewID").addClass("hide")
+    $("#addID").addClass("hide")
+    $("#verifyID").addClass("hide")
+    $("#updateID").addClass("hide")
+    $("#deleteID").addClass("hide")
+    $("#inputLine").removeClass("hide")
+    $("#verifyTextID").removeClass("hide")
+    let search = $("#verifyText").val()
+    for (let i = 0; i < employeeList.length; i++) {
+        if (search === employeeList[i].name) {
+            const updateName = $(".nameInput").val()
+            const updateOffice = $(".officeInput").val()
+            const updatePhone = $(".phoneInput").val()
+            const updateEmployee = {
+                'name': `${updateName}`,
+                'officeNum': `${updateOffice}`,
+                'phoneNum': `${updatePhone}`,
+            }
+            employeeList[i] = updateEmployee
+        } else { }
+    }
+}
+
+$(".update").on("click", showUpdate)
