116a99f6a3a14f19bd6f6f8f8e76ef8b542b801b
Tops <mtoplisek95@gmail.com>
2018-10-06 21:11:33 -0400

    got the add name button to start working and made the app not keep adding onto what had already been appended

--- app.js
+++ app.js
@@ -1,28 +1,54 @@
-$(".view").on("click", function () {
-    $(".mainContain").empty();
+const showView = function() {
+    $(".viewBtn").empty()
+    $("#viewID").removeClass("hide")
+    $("#addID").addClass("hide")
+    $("#verifyID").addClass("hide")
+    $("#updateID").addClass("hide")
+    $("#deleteID").addClass("hide")
+    $("#inputLine").addClass("hide")
     for (let i = 0; i < employeeList.length; i++) {
         const nameShow = employeeList[i].name
         const officeShow = employeeList[i].officeNum
         const phoneShow = employeeList[i].phoneNum
         $(".viewBtn").append(`<p>Name: ${nameShow}<br />Office Number: ${officeShow}<br />Phone Number: ${phoneShow}</p>`);
     }
-})
+}
 
-$(".add").on("click", function () {
-    $(".mainContain").empty();
+$(".view").on("click", showView)
+
+const showAdd = function () {
+    $(".addBtn").empty()
+    $("#viewID").addClass("hide")
+    $("#addID").removeClass("hide")
+    $("#verifyID").addClass("hide")
+    $("#updateID").addClass("hide")
+    $("#deleteID").addClass("hide")
+    $("#inputLine").removeClass("hide") 
     for (let i = 0; i < employeeList.length; i++) {
         const nameShow = employeeList[i].name
         const officeShow = employeeList[i].officeNum
         const phoneShow = employeeList[i].phoneNum
         $(".addBtn").append(`<p>Name: ${nameShow}<br />Office Number: ${officeShow}<br />Phone Number: ${phoneShow}</p>`);
-    }
+    }  
     const newName = $(".nameInput").val()
     const newOffice = $(".officeInput").val()
     const newPhone = $(".phoneInput").val()
     const newEmployee = {
-        "name": newName,
-        "officeNum": newOffice,
-        "phoneNum": newPhone,
+        'name': `${newName}`,
+        'officeNum': `${newOffice}`,
+        'phoneNum': `${newPhone}`,
     }
-    employeeList.push(newEmployee)
-})
+    employeeList.push(`${newEmployee}`) 
+}
+
+$(".add").on("click", showAdd)
+
+const showVerify = function() {
+    $(".verifyBtn").empty()
+    $("#viewID").addClass("hide")
+    $("#addID").removeClass("hide")
+    $("#verifyID").removeClass("hide")
+    $("#updateID").addClass("hide")
+    $("#deleteID").addClass("hide")
+    $("#inputLine").addClass("hide") 
+}
